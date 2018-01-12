1d9c2722bf79541a4b64a147c03af58ad4a2a70c
Artur Brodzki <artur.brodzki@gmail.com>
2018-01-12 21:09:18 +0100

    files activity layout

--- Android/app/src/main/java/com/mkoi/artur/mkoi/FilesActivity.java
+++ Android/app/src/main/java/com/mkoi/artur/mkoi/FilesActivity.java
@@ -15,9 +15,6 @@ public class FilesActivity extends AppCompatActivity {
         // Get the Intent that started this activity and extract the string
         Intent intent = getIntent();
         String message = intent.getStringExtra(LoginActivity.EXTRA_MESSAGE);
-
-        // Capture the layout's TextView and set the string as its text
-        TextView textView = findViewById(R.id.textView);
-        textView.setText(message);
+        
     }
 }
