e5cbe46b09df80f71a2615c81f3eaa7d74d677a5
Artur Brodzki <artur.brodzki@gmail.com>
2018-01-12 20:45:03 +0100

    template of android app

---
+++ Android/app/src/main/java/com/mkoi/artur/mkoi/FilesActivity.java
@@ -0,0 +1,23 @@
+package com.mkoi.artur.mkoi;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class FilesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_files);
+
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(LoginActivity.EXTRA_MESSAGE);
+
+        // Capture the layout's TextView and set the string as its text
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(message);
+    }
+}
